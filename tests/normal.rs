e5117e2f0e0fa01978dc84b930167990be5de4b8
Cody P Schafer <dev@codyps.com>
2017-02-11 01:53:11 -0500

    move tests to tests/normal.rs as they do not use internal details, so we can check the api

---
+++ tests/normal.rs
@@ -0,0 +1,72 @@
+extern crate leak;
+use leak::Leak;
+
+#[test]
+fn leak_str() {
+    use std::borrow::ToOwned;
+
+    let v = "hi";
+    let leaked : &str = {
+        let o = v.to_owned();
+        o.leak()
+    };
+    assert_eq!(leaked, v);
+
+    let leaked : &'static str = {
+        let o = v.to_owned();
+        o.leak()
+    };
+    assert_eq!(leaked, v);
+}
+
+#[test]
+fn leak_empty_str() {
+    use std::borrow::ToOwned;
+
+    let v = "";
+    let leaked : &'static str = {
+        let o = v.to_owned();
+        o.leak()
+    };
+    assert_eq!(leaked, v);
+}
+
+#[test]
+fn leak_vec() {
+    let v = vec![3, 5];
+    let leaked : &'static [u8] = {
+        let o = v.clone();
+        o.leak()
+    };
+    assert_eq!(leaked, &*v);
+}
+
+#[test]
+fn leak_empty_vec() {
+    let v = vec![];
+    let leaked : &'static [u8] = {
+        let o = v.clone();
+        o.leak()
+    };
+    assert_eq!(leaked, &*v);
+}
+
+#[test]
+fn leak_box() {
+    let v : Box<[&str]> = vec!["hi", "there"].into_boxed_slice();
+    let leaked : &'static [&str] = {
+        let o = v.clone();
+        o.leak()
+    };
+    assert_eq!(leaked, &*v);
+}
+
+#[test]
+fn leak_nested() {
+    let v : Box<Vec<&str>> = Box::new(vec!["hi", "there"]);
+    let leaked : &'static [&str] = {
+        let o = v.clone();
+        o.leak()
+    };
+    assert_eq!(leaked, &**v);
+}
